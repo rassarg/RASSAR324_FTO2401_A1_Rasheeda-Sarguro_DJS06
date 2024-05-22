e5946813f2e43e78736cce67931a56e2b61d0b25
rasheeda <raskieda@gmail.com>
2024-05-22 16:42:59 +0200

    exercise 6

--- exercises.js
+++ exercises.js
@@ -30,47 +30,58 @@ const products = [
 
 // ************************************** EXERCISES ************************************* //
 
-/*(1) ForEach Basics: Use forEach to log each name and each province to the console. Then, log each name with a matching province in the format "Name (Province)" */
-names.forEach((name) => {
-  console.log(`Exercise 1 - Name: ${name}`);
-});
-// names.forEach((name) => console.log(`Task 1 - Name: ${name}`));
+//(1) ForEach Basics:
 
-provinces.forEach((province) => {
-  console.log(`Exercise 1 - Province:${province}`);
-});
+// Log each name to the console
+names.forEach((name) => console.log(`Exercise 1 - Name: ${name}`));
 
-names.forEach((name, index) => {
-  console.log(`Exercise 1 - Name: ${name} (${provinces[index]})`);
-});
+// Log each province to the console
+provinces.forEach((province) =>
+  console.log(`Exercise 1 - Province: ${province}`)
+);
 
-/*(2) Uppercase Transformation: Use map to create a new array of province names in all uppercase. Log the new array to the console */
-const provinceNamesUppercase = provinces.map((province) => {
-  const provinceUpperCase = province.toUpperCase();
-  console.log(`Exercise 2 - ${provinceUpperCase}`);
-});
+// Log each name with a matching province in the format "Name (Province)"
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`));
 
-/*(3) Name Lengths: Create a new array using map that contains the length of each name */
+//(2) Uppercase Transformation:
+
+// Use map to create a new array of province names in all uppercase
+const provinceNamesUppercase = provinces.map((province) =>
+  province.toUpperCase()
+);
+console.log(`Exercise 2 - ${provinceNamesUppercase}`); // Log the new array to the console
+
+//(3) Name Lengths:
+
+// Create a new array using map that contains the length of each name
 const nameAndLengths = names.map((name) => {
   const nameLengths = name.length;
   console.log(`Exercise 3 - ${name}: ${nameLengths}`);
 });
 
-/*(4) Sorting: Use sort to alphabetically sort the provinces.*/
+//(4) Sorting:
+
+// Use sort to alphabetically sort the provinces
 provinces.sort();
 console.log(`Exercise 4 - ${provinces}`);
 
-/*(5) Filtering Cape: Use filter to remove provinces containing "Cape". Log the count of remaining provinces. */
+//(5) Filtering Cape:
+
+// Use filter to remove provinces containing "Cape"
 const provincesWithoutCape = provinces.filter(
   (province) => !province.includes("Cape")
 );
+// Log the count of remaining provinces
 console.log(
   `Exercise 5 - Number of provinces without 'Cape' = ${provincesWithoutCape.length}`
 );
 
-/*(6) Finding 'S': Create a boolean array using map and some to determine if a name contains the letter 'S'. */
+//(6) Finding 'S': Create a boolean array using map and some to determine if a name contains the letter 'S'
+const namesWithS = names.map((name) => name.toLowerCase().includes("s"));
+const nameHasS = namesWithS.some((item) => item);
+console.log(`Exercise 6 -`, names, namesWithS);
 
-/*(7) Creating Object Mapping: Use reduce to transform the names array into an object mapping names to their respective provinces. */
+//(7) Creating Object Mapping: Use reduce to transform the names array into an object mapping names to their respective provinces. */
 
 // products.forEach((product) => {
 //   console.log(product.product);
